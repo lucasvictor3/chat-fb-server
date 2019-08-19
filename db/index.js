70e26126f43cf0baeeddefe0900e9d91949a2d94
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2019-08-19 00:02:46 -0300

    changing package and removing trash files

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const uri = 'mongodb://localhost/noderest';
+//password: qK6Z5LPqyznE31RK
+//const uri = 'mongodb://localhost/noderest';
+const uri = "mongodb+srv://lucasvictor3:qK6Z5LPqyznE31RK@cluster0-uxydw.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect( uri , {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
