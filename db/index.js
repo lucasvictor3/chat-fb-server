2da25d6c15cd513e3116eef17be8d40e02028920
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2019-08-19 09:17:41 -0300

    test

--- db/index.js
+++ db/index.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-//password: qK6Z5LPqyznE31RK
-//const uri = 'mongodb://localhost/noderest';
-
 
 
 const uri = process.env.MONGODB_URI;
+console.log(process.env.MONGODB_URI)
 mongoose.connect( uri , {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
